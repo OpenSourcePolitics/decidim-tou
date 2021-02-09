61862cae588581bf357aa2ef2a90071c9062200c
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2021-02-09 17:44:27 +0100

    backport # 862 from opensourcepolitics/decidim

--- db/migrate/20191211133811_add_registration_metadata_to_decidim_users.rb
+++ db/migrate/20191211133811_add_registration_metadata_to_decidim_users.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-# This migration comes from decidim (originally 20191211133811)
 
 class AddRegistrationMetadataToDecidimUsers < ActiveRecord::Migration[5.2]
   def change
---
+++ spec/forms/registration_form_spec.rb
@@ -0,0 +1,201 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+module Decidim
+  describe RegistrationForm do
+    subject do
+      described_class.from_params(
+          attributes
+      ).with_context(
+          context
+      )
+    end
+
+    let(:organization) { create(:organization) }
+    let(:name) { "User" }
+    let(:nickname) { "justme" }
+    let(:email) { "user@example.org" }
+    let(:password) { "S4CGQ9AM4ttJdPKS" }
+    let(:password_confirmation) { password }
+    let(:tos_agreement) { "1" }
+    let(:residential_area) { create(:scope, organization: organization).id.to_s }
+    let(:work_area) { create(:scope, organization: organization).id.to_s }
+    let(:gender) { "female" }
+    let(:month) { "January" }
+    let(:year) { "1992" }
+    let(:underage) { true }
+    let(:statutory_representative_email) { "statutory-representative@example.org" }
+
+    let(:attributes) do
+      {
+          name: name,
+          nickname: nickname,
+          email: email,
+          password: password,
+          password_confirmation: password_confirmation,
+          tos_agreement: tos_agreement,
+          residential_area: residential_area,
+          work_area: work_area,
+          gender: gender,
+          month: month,
+          year: year,
+          underage: underage,
+          statutory_representative_email: statutory_representative_email
+      }
+    end
+
+    let(:context) do
+      {
+          current_organization: organization
+      }
+    end
+
+    context "when everything is OK" do
+      it { is_expected.to be_valid }
+    end
+
+    context "when the email is a disposable account" do
+      let(:email) { "user@mailbox92.biz" }
+
+      it { is_expected.to be_invalid }
+    end
+
+    context "when the name is not present" do
+      let(:name) { nil }
+
+      it { is_expected.to be_invalid }
+    end
+
+    context "when the nickname is not present" do
+      let(:nickname) { nil }
+
+      it { is_expected.to be_invalid }
+    end
+
+    context "when the email is not present" do
+      let(:email) { nil }
+
+      it { is_expected.to be_invalid }
+    end
+
+    context "when the email already exists" do
+      let!(:user) { create(:user, organization: organization, email: email) }
+
+      it { is_expected.to be_invalid }
+    end
+
+    context "when the nickname already exists" do
+      let!(:user) { create(:user, organization: organization, nickname: nickname) }
+
+      it { is_expected.to be_invalid }
+    end
+
+    context "when the nickname is too long" do
+      let(:nickname) { "verylongnicknamethatcreatesanerror" }
+
+      it { is_expected.to be_invalid }
+    end
+
+    context "when the password is not present" do
+      let(:password) { nil }
+
+      it { is_expected.to be_invalid }
+    end
+
+    context "when the password is weak" do
+      let(:password) { "aaaabbbbcccc" }
+
+      it { is_expected.to be_invalid }
+    end
+
+    context "when the password confirmation is not present" do
+      let(:password_confirmation) { nil }
+
+      it { is_expected.to be_invalid }
+    end
+
+    context "when the password confirmation is different from password" do
+      let(:password_confirmation) { "invalid" }
+
+      it { is_expected.to be_invalid }
+    end
+
+    context "when the tos_agreement is not accepted" do
+      let(:tos_agreement) { "0" }
+
+      it { is_expected.to be_invalid }
+    end
+
+    context "when residential_area is not a scope" do
+      let(:residential_area) { "23" }
+
+      it { is_expected.to be_invalid }
+    end
+
+    context "when residential_area is nil" do
+      let(:residential_area) { nil }
+
+      it { is_expected.to be_invalid }
+    end
+
+    context "when work_area is not a scope" do
+      let(:work_area) { "abcd" }
+
+      it { is_expected.to be_invalid }
+    end
+
+    context "when work_area is nil" do
+      let(:work_area) { nil }
+
+      it { is_expected.to be_valid }
+    end
+
+    context "when gender is not in list" do
+      let(:gender) { "Apache helicopter" }
+
+      it { is_expected.to be_invalid }
+    end
+
+    context "when gender is nil" do
+      let(:gender) { nil }
+
+      it { is_expected.to be_valid }
+    end
+
+    context "when birth_date year is not in the range" do
+      let(:year) { "1000" }
+
+      it { is_expected.to be_invalid }
+    end
+
+    context "when birth_date month is not in the list" do
+      let(:month) { "Apple" }
+
+      it { is_expected.to be_invalid }
+    end
+
+    context "when underage is unchecked" do
+      let(:underage) { nil }
+
+      it { is_expected.to be_valid }
+    end
+
+    context "when age is under than 15 yo" do
+      let(:month) { "June" }
+      let(:year) { 2020 }
+
+      context "and underage is unchecked" do
+        let(:underage) { nil }
+
+        it { is_expected.to be_valid }
+      end
+    end
+
+    context "when underage is checked but statutory_representative_email is nil" do
+      let(:statutory_representative_email) { nil }
+
+      it { is_expected.to be_invalid }
+    end
+  end
+end
