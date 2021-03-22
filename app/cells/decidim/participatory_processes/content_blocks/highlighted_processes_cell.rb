a3594f46c22a4b40f3a01e0b78c4126e21c0f01b
Quentin Champ <26109239+Quentinchampenois@users.noreply.github.com>
2021-03-22 17:10:43 +0100

    Backport linked assemblies (#10)
    * add pp specs
    
    * quick fix I18n specs in cells
    
    * backport linked assemblies in content block
    
    * revert highlighted processes before accessibility fix
    
    * add linked_assemblies_for method
    
    * add related assemblies to pp banner
    
    * backport Add linked assemblies to process group
    
    * Revert "quick fix I18n specs in cells"
    
    This reverts commit bfd0b5866a8390d28390b047b66a5804d2f8efa0.
    
    * lint
    
    * add missing keys
    
    * define linked_assemblies_for method as helper
    
    * restrict assemblies to public spaces

---
+++ app/cells/decidim/participatory_processes/content_blocks/highlighted_processes_cell.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+module Decidim
+  module ParticipatoryProcesses
+    module ContentBlocks
+      class HighlightedProcessesCell < Decidim::ViewModel
+        include Decidim::SanitizeHelper
+
+        delegate :current_user, to: :controller
+
+        def show
+          if single_process?
+            render "single_process"
+          elsif highlighted_processes.any?
+            render
+          end
+        end
+
+        def single_process?
+          highlighted_processes.to_a.length == 1
+        end
+
+        def max_results
+          model.settings.max_results
+        end
+
+        def highlighted_processes
+          @highlighted_processes ||= (
+            OrganizationPublishedParticipatoryProcesses.new(current_organization, current_user) |
+            HighlightedParticipatoryProcesses.new |
+            FilteredParticipatoryProcesses.new("active")
+          ).query.includes([:organization]).limit(max_results)
+        end
+
+        def linked_assemblies_for(process)
+          process.linked_participatory_space_resources(:assembly, "included_participatory_processes").published
+        end
+
+        def i18n_scope
+          "decidim.participatory_processes.pages.home.highlighted_processes"
+        end
+
+        def decidim_participatory_processes
+          Decidim::ParticipatoryProcesses::Engine.routes.url_helpers
+        end
+
+        def decidim_assemblies
+          Decidim::Assemblies::Engine.routes.url_helpers
+        end
+      end
+    end
+  end
+end
--- spec/factories.rb
+++ spec/factories.rb
@@ -4,8 +4,8 @@ require "decidim/core/test/factories"
 
 # Factories
 
-# require "decidim/participatory_processes/test/factories"
-# require "decidim/proposals/test/factories"
+require "decidim/participatory_processes/test/factories"
+require "decidim/proposals/test/factories"
 # require "decidim/meetings/test/factories"
 # require "decidim/accountability/test/factories"
 require "decidim/system/test/factories"
