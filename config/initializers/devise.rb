e57822461bc4bc282c6a5aeda7067396c0ab2f3b
Armand Fardeau <armandfardeau@users.noreply.github.com>
2021-05-06 12:21:27 +0200

    Delete invitations instructions (#27)
    * Delete invitations instructions
    
    * Delete related translations and invite config (#29)
    
    * Delete related translations and invite config
    
    * fix failing tests
    
    Co-authored-by: Pauline Bessoles <52420208+paulinebessoles@users.noreply.github.com>

--- config/initializers/devise.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# Use this hook to configure devise mailer, warden hooks and so forth.
-# Many of these configuration options can be set straight in your model.
-
-Devise.setup do |config|
-  config.invite_for = 1.month
-end
