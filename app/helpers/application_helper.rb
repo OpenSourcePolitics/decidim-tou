5574f693755365e159a7c4f70562d41df8765d0f
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2021-06-23 10:24:42 +0200

    fix undefined display_emitter method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,38 +1,9 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  include EmitterHelper
+
   def linked_assemblies_for(process)
     process.linked_participatory_space_resources(:assembly, "included_participatory_processes").public_spaces
-  end
-
-  def display_emitter(process)
-    return if process.emitter == "unspecified"
-
-    {
-      picture: render_picture(process.emitter),
-      text: t("emitter_text",
-              developer_group: translated_attribute(process.developer_group),
-              scope: "decidim.participatory_processes.emitter")
-    }
-  end
-
-  private
-
-  def render_picture(emitter)
-    if emitter == "city"
-      city_picture
-    elsif emitter == "metropolis"
-      metropolis_picture
-    else
-      metropolis_picture.concat(city_picture)
-    end
-  end
-
-  def metropolis_picture
-    image_tag("toulouse/logo-metropole-grey.png")
-  end
-
-  def city_picture
-    image_tag("toulouse/logo-mairie-noir.png")
   end
 end
