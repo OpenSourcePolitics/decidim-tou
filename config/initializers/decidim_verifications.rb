27f7cd3e06b46612c4b304dcc2e32bdb34dc47c6
Armand Fardeau <fardeauarmand@gmail.com>
2021-07-01 19:34:34 +0200

    Add SMS gateway service

--- config/initializers/decidim_verifications.rb
+++ config/initializers/decidim_verifications.rb
@@ -22,3 +22,9 @@
 Decidim::Verifications.register_workflow(:osp_authorization_handler) do |auth|
   auth.form = "Decidim::OspAuthorizationHandler"
 end
+
+Rails.application.initializer "sms_verification_workflow", after: "decidim.sms_verification_workflow" do
+  if Decidim.sms_gateway_service
+    Decidim::Verifications.find_workflow_manifest(:sms).expires_in = 5.minutes
+  end
+end
