b20d8e5c4888bb0a6ba62276f645bbf817e80dfd
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-07-07 16:11:00 +0200

    Add helpers of participatory processes

---
+++ app/helpers/decidim/participatory_processes/application_helper.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Decidim
+  module ParticipatoryProcesses
+    # Custom helpers, scoped to the participatory processes engine.
+    #
+    module ApplicationHelper
+      include Decidim::ResourceHelper
+    end
+  end
+end
