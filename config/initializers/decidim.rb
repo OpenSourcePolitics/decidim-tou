762e314e72b217a3997e469a995a52f9d2efa458
Armand Fardeau <armandfardeau@users.noreply.github.com>
2022-07-12 16:30:51 +0200

    Backport https://github.com/OpenSourcePolitics/decidim-tou/pull/41 (#69)
    * Backport https://github.com/OpenSourcePolitics/decidim-tou/pull/41
    
    * Update locales
    
    Co-authored-by: paulinebessoles <pauline.bessoles@opensourcepolitics.eu>

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -85,7 +85,7 @@ Decidim.configure do |config|
   #   end
   # end
   #
-  # config.sms_gateway_service = 'Decidim::Verifications::Sms::ExampleGateway'
+  config.sms_gateway_service = "SMSGatewayService"
 
   # Etherpad configuration
   #
