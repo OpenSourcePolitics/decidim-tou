bcfa5fdf6416f6d1bf529d74d2a9cab5d67e6894
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2022-07-12 15:56:38 +0200

    Light enhancements

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -11,7 +11,7 @@ Decidim.configure do |config|
     config.available_locales = [:fr, :en, :ca, :es]
   else
     config.default_locale = :fr
-    config.available_locales = [:fr, :en]
+    config.available_locales = [:fr]
   end
 
   # Timeout session
--- config/initializers/extends.rb
+++ config/initializers/extends.rb
@@ -5,6 +5,7 @@ require "extends/commands/decidim/create_registration"
 
 if Dir.glob("lib/extends/exporters/*.rb").blank?
   Rails.logger.warn("Extends not found (Looking at 'lib/extends/exporters/*.rb')")
+  Rails.logger.warn("Exports columns name won't be translated")
 else
   Dir.glob("lib/extends/exporters/*.rb").each { |f| load f }
 end
