8d417dbff421bbea635537b020b466051d82046b
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2022-08-03 09:06:11 +0200

    Update migration to be reversible

--- db/migrate/20220718142533_modify_emitter_in_participatory_processes.rb
+++ db/migrate/20220718142533_modify_emitter_in_participatory_processes.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 class ModifyEmitterInParticipatoryProcesses < ActiveRecord::Migration[6.0]
-  def change
+  def up
     change_column :decidim_participatory_processes, :emitter, :string
 
     add_column :decidim_participatory_processes, :emitter_name, :text
+  end
+
+  def down
   end
 end
