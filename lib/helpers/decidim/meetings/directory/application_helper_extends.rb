b64f7b38fb29b2727359e3dcbf3dc7528a3fc7c5
AyakorK <guillaume.moret@yahoo.com>
2023-03-06 16:43:15 +0100

    Sort children on meetings directory page by extending helper

---
+++ lib/helpers/decidim/meetings/directory/application_helper_extends.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module Decidim
+  module Meetings
+    module Directory
+      module ApplicationHelperExtends
+        include Decidim::CheckBoxesTreeHelper
+        def scope_children_to_tree(scope)
+          return unless scope.children.any?
+
+          sorted_children = sort_children(scope.children)
+
+          sorted_children.flat_map do |child|
+            TreeNode.new(
+              TreePoint.new(child.id.to_s, translated_attribute(child.name, current_organization)),
+              scope_children_to_tree(child)
+            )
+          end
+        end
+
+        def sort_children(children)
+          children.sort_by { |child| child.name.values.first.split.first.to_f }
+        end
+      end
+    end
+  end
+end
+
+Decidim::Meetings::Directory::ApplicationHelper.module_eval do
+  prepend(Decidim::Meetings::Directory::ApplicationHelperExtends)
+end
