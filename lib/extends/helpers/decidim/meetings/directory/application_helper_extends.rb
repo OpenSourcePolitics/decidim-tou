e5837607e08a0fbff2252d3e9f5ef8765a5e2ff9
quentinchampenois <quentin.champenois7@gmail.com>
2023-03-22 15:36:45 +0100

    Ensure default I18n locale is a String

--- lib/extends/helpers/decidim/meetings/directory/application_helper_extends.rb
+++ lib/extends/helpers/decidim/meetings/directory/application_helper_extends.rb
@@ -8,7 +8,7 @@ module Decidim
         def scope_children_to_tree(scope)
           return unless scope.children.any?
 
-          sorted_children = scope.children.sort_by { |subscope| subscope.name[I18n.locale.to_s] || subscope.name[I18n.default_locale] }
+          sorted_children = scope.children.sort_by { |subscope| subscope.name[I18n.locale.to_s] || subscope.name[I18n.default_locale.to_s] }
           sorted_children.flat_map do |child|
             TreeNode.new(
               TreePoint.new(child.id.to_s, translated_attribute(child.name, current_organization)),
