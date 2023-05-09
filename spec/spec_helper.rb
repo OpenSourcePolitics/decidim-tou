ed83c55655f33082ced910e67948422172301901
Elie Gaboriau <elie@opensourcepolitics.eu>
2023-05-09 14:36:14 +0200

    preparation ih

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,12 @@ Decidim::Dev.dummy_app_path = File.expand_path(Rails.root.to_s)
 require "decidim/dev/test/base_spec_helper"
 require "decidim/dev/test/w3c_rspec_validators_overrides"
 
+Dir.glob("./spec/support/**/*.rb").sort.each { |f| require f }
+
 RSpec.configure do |config|
+  config.formatter = ENV.fetch("RSPEC_FORMAT", "progress").to_sym
+  config.include EnvironmentVariablesHelper
+
   config.before do
     # Initializers configs
     SocialShareButton.configure do |social_share_button|
