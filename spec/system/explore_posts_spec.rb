bc87c708f8e00eab810572856f5639b26d78cf0c
AyakorK <guillaume.moret@yahoo.com>
2023-09-14 11:48:56 +0200

    Rubocop fixes

--- spec/system/explore_posts_spec.rb
+++ spec/system/explore_posts_spec.rb
@@ -5,12 +5,11 @@ require "spec_helper"
 describe "Explore posts", type: :system do
   include_context "with a component"
   let(:manifest_name) { "blogs" }
-  Time.zone = "UTC"
-
   let!(:old_post) { create(:post, component: component, created_at: Time.current - 2.days) }
   let!(:new_post) { create(:post, component: component, created_at: Time.current) }
-
   let!(:image) { create(:attachment, attached_to: old_post) }
+
+  Time.zone = "UTC"
 
   describe "index" do
     it "shows all posts for the given process" do
