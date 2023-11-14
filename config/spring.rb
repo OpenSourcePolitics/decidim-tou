8dcfb62db549d944d866febb91182910563ad13c
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2023-11-14 15:07:55 +0100

    update: Config spring

--- config/spring.rb
+++ config/spring.rb
@@ -1,8 +1,8 @@
-# frozen_string_literal: true
+require "decidim/spring"
 
-%w(
-  .ruby-version
-  .rbenv-vars
-  tmp/restart.txt
-  tmp/caching-dev.txt
-).each { |path| Spring.watch(path) }
+Spring.watch(
+  ".ruby-version",
+  ".rbenv-vars",
+  "tmp/restart.txt",
+  "tmp/caching-dev.txt"
+)
