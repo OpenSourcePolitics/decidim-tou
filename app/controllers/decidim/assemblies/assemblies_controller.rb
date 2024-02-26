e1140d7f305f35982402ecb1f523cd76b67092ce
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2024-02-26 12:25:20 +0100

    fix: Sort active PPs by end_date desc

--- app/controllers/decidim/assemblies/assemblies_controller.rb
+++ app/controllers/decidim/assemblies/assemblies_controller.rb
@@ -88,7 +88,7 @@ module Decidim
         assembly_participatory_processes = @current_participatory_space.linked_participatory_space_resources(:participatory_processes, "included_participatory_processes")
 
         sorted_by_date = {
-          active: assembly_participatory_processes.active_spaces.order(start_date: :desc),
+          active: assembly_participatory_processes.active_spaces.order(end_date: :desc),
           future: assembly_participatory_processes.future_spaces.order(start_date: :asc),
           past: assembly_participatory_processes.past_spaces.order(end_date: :desc)
         }
