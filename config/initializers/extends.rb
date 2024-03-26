6cd9ccd9c370600e4cfed0255bf91d63e09157d4
AyakorK <guillaume.moret@yahoo.com>
2024-03-26 14:47:13 +0100

    fix: Remove the extend to fix the issue that we had

--- config/initializers/extends.rb
+++ config/initializers/extends.rb
@@ -3,4 +3,3 @@
 require "extends/controllers/decidim/devise/account_controller_extends"
 require "extends/cells/decidim/content_blocks/hero_cell_extends"
 require "extends/uploaders/decidim/application_uploader_extends"
-require "extends/lib/decidim/forms/user_answers_serializer_extend"
