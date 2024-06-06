50c55e7b4e2bd238aae9e6c620c18deca4c886eb
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2024-06-06 10:43:54 +0200

    fix: override PP form

--- app/forms/decidim/participatory_processes/admin/participatory_process_form.rb
+++ app/forms/decidim/participatory_processes/admin/participatory_process_form.rb
@@ -7,6 +7,7 @@ module Decidim
       # dashboard.
       #
       class ParticipatoryProcessForm < Form
+        # include Decidim::Emitter::EmitterHelper
         include TranslatableAttributes
         include Decidim::HasUploadValidations
 
@@ -54,6 +55,17 @@ module Decidim
         attribute :remove_banner_image, Boolean, default: false
         attribute :remove_hero_image, Boolean, default: false
 
+        attribute :emitter
+        attribute :emitter_name
+        attribute :emitter_image
+        attribute :emitter_select
+        attribute :emitter_name_image
+        attribute :emitter_name_select
+        attribute :emitter_read_name
+        attribute :remove_emitter, Decidim::AttributeObject::TypeMap::Boolean, default: false
+        attribute :remove_emitter_image, Decidim::AttributeObject::TypeMap::Boolean, default: false
+
+        validates :emitter, passthru: { to: Decidim::ParticipatoryProcess }
         validates :address, geocoding: true, if: proc { |object| object.address.present? }
         validates :area, presence: true, if: proc { |object| object.area_id.present? }
         validates :scope, presence: true, if: proc { |object| object.scope_id.present? }
@@ -108,6 +120,61 @@ module Decidim
           end
         end
 
+        def remove_emitter_image=(value)
+          self.remove_emitter = ActiveModel::Type::Boolean.new.cast(value) if value
+          prepare_emitter_name!
+        end
+
+        def emitter_name_image=(value)
+          @emitter_name_image = value if value
+          prepare_emitter_name!
+          prepare_emitter!
+        end
+
+        def emitter_name_select=(value)
+          @emitter_name_select = value if value
+          prepare_emitter_name!
+        end
+
+        def emitter_image=(value)
+          @emitter_image = value if value
+          prepare_emitter!
+        end
+
+        def emitter_select=(value)
+          if value.present?
+            target_pp = Decidim::ParticipatoryProcess.find(value)
+            blob = target_pp.emitter.attachment.blob
+            @emitter_select = blob
+            @emitter_name_select = target_pp.emitter_name
+          end
+          prepare_emitter!
+          prepare_emitter_name!
+        end
+
+        def emitter_image
+          @emitter_image || emitter
+        end
+
+        def emitter_select
+          @emitter_select || emitter
+        end
+
+        attr_reader :emitter_name_image
+
+        def emitter_name_select
+          @emitter_name_select || emitter_name
+        end
+
+        def emitter_read_name
+          @emitter_read_name || emitter_name
+        end
+
+        def emitter_read_name=(value)
+          @emitter_read_name = value if value
+          prepare_emitter_name!
+        end
+
         private
 
         def participatory_process_types
@@ -125,6 +192,18 @@ module Decidim
                           .any?
 
           errors.add(:slug, :taken)
+        end
+
+        def prepare_emitter_name!
+          self.emitter_name = emitter_read_name
+          self.emitter_name = emitter_name_select if emitter_name_select.present?
+          self.emitter_name = emitter_name_image if emitter_name_image.present?
+          self.emitter_name = nil if remove_emitter
+        end
+
+        def prepare_emitter!
+          self.emitter = emitter_select if emitter_select
+          self.emitter = emitter_image if emitter_image
         end
       end
     end
