6323a5a01edb286c377835a6a8e141c17fd7863d
stephanie rousset <strabon@club-internet.fr>
2024-08-08 14:56:13 +0200

    test: add sleep to dismiss_page_upload and update options of selenium webdriver

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,12 +10,15 @@ Capybara.register_driver :headless_chrome do |app|
   options = ::Selenium::WebDriver::Chrome::Options.new
   options.args << "--headless=new"
   options.args << "--no-sandbox"
+  options.args << "--disable-gpu"
+  options.args << "--disable-popup-blocking"
   options.args << if ENV["BIG_SCREEN_SIZE"].present?
                     "--window-size=1920,3000"
                   else
                     "--window-size=1920,1080"
                   end
   options.args << "--ignore-certificate-errors" if ENV["TEST_SSL"]
+  options.add_preference("intl.accept_languages", "en-GB")
   Capybara::Selenium::Driver.new(
     app,
     browser: :chrome,
