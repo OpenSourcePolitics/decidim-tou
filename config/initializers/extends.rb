c54f7f364d1841cebc001151f1ac80ac8cfef5aa
AyakorK <guillaume.moret@yahoo.com>
2024-10-02 17:10:24 +0200

    feat: Remove Phone Authorization Handler Gem

--- config/initializers/extends.rb
+++ config/initializers/extends.rb
@@ -3,4 +3,3 @@
 require "extends/controllers/decidim/devise/account_controller_extends"
 require "extends/cells/decidim/content_blocks/hero_cell_extends"
 require "extends/uploaders/decidim/application_uploader_extends"
-require "extends/lib/decidim/phone_authorization_handler/proposal_serializer_extend"
